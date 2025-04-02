e4d2130fa5f4e62e13ae60f533a21111ca72fd41
Zikora <zikorapascal@gmail.com>
2025-04-02 04:15:13 +0100

    create dicee program

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let fruits = ['apple', 'orange', 'banana'];
+function roll(){
+    let inp = document.getElementById('input');
+    let output = document.getElementById('output');
+    let dice = document.getElementById('dices');
+    
+    let value = [];
+    let img = [];
 
-for (let i = 0 ; i < fruits.length; i++){
-    console.log(fruits[i]);
+    for(let i = 0; i < inp.value; i++){
+        let num = Math.floor(Math.random() * 6) + 1;
+        value.push( num);
+        img.push(`<img src="/story/images/dice${num}.png" alt="">`);
+
+    }
+    output.append(value.join(', '));
+    dice.innerHTML = img;
 }
